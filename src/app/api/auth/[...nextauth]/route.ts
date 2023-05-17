cb041a412b6515afca1b94a5b121d244b9ea897e
lasantha viraj dissanayaka <slinfotecsoftware@gmail.com>
2023-05-17 14:01:15 +0530

    url changed

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -15,7 +15,7 @@ const handler = NextAuth({
       },
       async authorize(credentials, req) {
         // Add logic here to look up the user from the credentials supplied
-        const res = await fetch("http://localhost:3000/api/login", {
+        const res = await fetch("https://next-auth1.netlify.app/api/login", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
